f9973c3901dff98d812e7d67d6e2c0d9ce8daadf
Sejal075 <sejalru235@gmail.com>
2026-02-05 11:13:27 +0530

    --- Auto Git Commit ---

--- Day3/modules/ec2/main.tf
+++ Day3/modules/ec2/main.tf
@@ -6,8 +6,38 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = [
     var.security_group_id
   ]
+  user_data = <<-EOF
+    #!/bin/bash
+
+    # Update system
+    yum update -y
+
+    # Install required packages
+    yum install -y git curl
+
+    # Install NVM
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    export NVM_DIR="/home/ec2-user/.nvm"
+    source /home/ec2-user/.nvm/nvm.sh
+
+    # Install Node.js 20
+    nvm install 20
+    nvm use 20
+    nvm alias default 20
+
+    # Install Yarn
+    npm install -g yarn
+
+    # Create Strapi application
+    cd /home/ec2-user
+    npx create-strapi-app@latest my-strapi --quickstart
+
+    # Fix ownership
+    chown -R ec2-user:ec2-user /home/ec2-user/my-strapi
+  EOF
 
     tags = {
     Name = "Strapi-EC2"
   }
 }
+
