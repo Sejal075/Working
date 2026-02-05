828ffe5db801a9a4478be5d9165efd9cfea1860b
Sejal075 <sejalru235@gmail.com>
2026-02-05 23:09:45 +0530

    --- Auto Git Commit ---

--- Day4/variable.tf
+++ Day4/variable.tf
@@ -1,43 +1,36 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-2"
 }
 
 variable "env" {
   description = "Environment name"
-  default = "dev"
 }
 
 variable "vpc_cidr" {
   description = "VPC CIDR"
-  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "Public subnet CIDR"
-  default = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "Private subnet CIDR"
-  default = "10.0.2.0/24"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  default = "t3.micro"
 }
 
 variable "key_name" {
   description = "EC2 key pair name"
-  default = "keypair"
 }
 
 variable "allowed_ssh_cidr" {
   description = "Your IP for SSH"
-  default = ""
 }
 
 variable "strapi_port" {
   default = 1337
 }
+
